555843043954df3b30778471d8f3df1f3d1f42a5
Pete Kutrumbos <peter.kutrumbos@gmail.com>
2020-04-25 10:42:35 -0700

    Clean up sign-up page

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.first_name }
     email { Faker::Internet.email }
+    password { Faker::Alphanumeric.alphanumeric(number: 16) }
   end
 end
